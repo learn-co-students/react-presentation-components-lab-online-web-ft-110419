27afdf3bdef6fa5ccb6bdf1a97113d6309f2332f
LucyWeltner <lucyiweltner@gmail.com>
2020-03-25 10:12:02 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
-// Code SimpleComponent Here
+import React from 'react'
+export default class SimpleComponent extends React.Component {
+	state = {
+		mood: "happy"
+	}
+
+	handleClick = () => {
+		this.setState(prevState => 
+			{	
+				if (prevState.mood === "happy") {
+					return {mood: "sad"}
+				}
+				else {
+					return {mood: "happy"}
+				}
+			}
+		)
+	}
+
+	render(){
+		return (
+			<div onClick = {this.handleClick}>{this.state.mood}</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => {event.target.style.background = (event.target.style.background === "rgb(50, 200, 200)" ? "rgb(180, 0, 200)" : "rgb(50, 200, 200)")}} />
   </div>,
   document.getElementById('root')
 );
