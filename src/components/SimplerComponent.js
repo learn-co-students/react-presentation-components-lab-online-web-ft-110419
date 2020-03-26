59f146e494a2639e0208f69d80003b596f5330a7
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-03-25 21:24:20 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,6 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = props => <div onClick={props.handleCLick} style={{color: "#f00"}}>I am just happy.</div>
+
+export default SimplerComponent;
