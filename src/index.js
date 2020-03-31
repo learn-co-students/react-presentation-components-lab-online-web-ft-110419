71459dd80a6713b65fd59241a2dc5ccc0fef7521
Ethan <ethan_gustafson@outlook.com>
2020-03-31 15:40:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const moodChange = () => alert("I am sad")
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={moodChange} />
   </div>,
   document.getElementById('root')
 );
